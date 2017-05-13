be3ea693c6b86909e6770669dc0702d1ab5390b3
potinglee <potinglee999@gmail.com>
2017-05-14 01:20:17 +0800

    original

---
+++ raytracing.h
@@ -0,0 +1,30 @@
+#ifndef __RAYTRACING_H
+#define __RAYTRACING_H
+
+#include "objects.h"
+#include <stdint.h>
+
+#define thread_num 64
+//
+// void raytracing_dowork(uint8_t *pixels, color background_color,
+//                 rectangular_node rectangulars, sphere_node spheres,
+//                 light_node lights, const viewpoint *view,
+//                 int width, int height);
+void *raytracing_dowork(void *inputdata);
+void raytracing(uint8_t *pixels, color background_color,
+                rectangular_node rectangulars, sphere_node spheres,
+                light_node lights, const viewpoint *view,
+                int width, int height);
+typedef struct {
+  uint8_t *pixels;
+  color background_color;
+  rectangular_node *rectangulars;
+  sphere_node *spheres;
+  light_node *lights;
+  const viewpoint *view;
+  int width;
+  int height;
+  int tag;
+}raytracing_thread_data;
+
+#endif
